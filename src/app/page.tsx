1c3d02d06f3b11488276fd6620c3a94ce8a1434c
nick-z-h <n.zhanghoule@gmail.com>
2024-08-17 19:48:26 -0400

    feat: fetch data from uploadthing and render markdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 import Link from "next/link";
+import { Suspense } from "react";
+import MarkdownFetcher from "~/components/ui/markdown-fetcher";
+
+const mockUrls = [
+  "https://utfs.io/f/3b264d81-4160-495c-ba00-62b29f52479a-w1l1jd.mdx",
+  "https://utfs.io/f/6e6833eb-104c-4ef6-9ec8-3f2f7ab42b62-w1l1jc.mdx",
+  "https://utfs.io/f/72dfa17e-e39b-439f-b69c-065861c56255-w1l1jb.mdx",
+  "https://utfs.io/f/03933592-e1c1-4abc-94fa-01dc8f40ecd4-fgnuj6.mdx",
+];
+
+const mockDocuments = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        Test
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        <Suspense fallback={<>Loading...</>}>
+          {mockDocuments.map((document) => (
+            <MarkdownFetcher key={document.id} url={document.url} />
+          ))}
+        </Suspense>
       </div>
     </main>
   );
