b3c59bc2d90925c763b9a82b586931e4fa10575f
nick-z-h <n.zhanghoule@gmail.com>
2024-08-17 19:08:38 -0400

    feat: create markdown fetch server component

---
+++ src/components/ui/markdown-fetcher.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+export default async function MarkdownFetcher({ url }: { url: string }) {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch the markdown file");
+  }
+
+  const markdown = await res.text();
+
+  return (
+    <article className="prose lg:prose-xl">
+      <MDXRemote source={markdown} />
+    </article>
+  );
+}
