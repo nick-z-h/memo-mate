cecea6787a3095e629c011ff82313281357d7f31
nick-z-h <n.zhanghoule@gmail.com>
2024-08-17 23:37:17 -0400

    feat: seperate components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,31 +6,47 @@ import MarkdownFetcher from "~/components/markdown-fetcher";
 
 import { Skeleton } from "~/components/ui/skeleton";
 import React from "react";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 // Page has dynamic data
 // => We want to ensure it is dynamically rendered
 export const dynamic = "force-dynamic";
 
-export default async function DashboardPage() {
+async function Documents() {
   const documents = await db.query.documents.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
   return (
+    <>
+      {documents.map((document) => (
+        <Card key={document.id} className="h-80 w-96">
+          <CardHeader>
+            <CardTitle>{document.title}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <article className="prose h-56 w-80 overflow-auto">
+              <MarkdownFetcher url={document.url} />
+            </article>
+          </CardContent>
+        </Card>
+      ))}
+    </>
+  );
+}
+
+export default function DashboardPage() {
+  return (
     <div className="flex flex-wrap gap-4 text-wrap p-4">
-      <Suspense fallback={<Skeleton className="h-80 w-96 rounded-xl" />}>
-        {documents.map((document) => (
-          <Card key={document.id} className="h-80 w-96">
-            <CardHeader>
-              <CardTitle>{document.title}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <article className="prose h-56 w-80 overflow-auto">
-                <MarkdownFetcher url={document.url} />
-              </article>
-            </CardContent>
-          </Card>
-        ))}
-      </Suspense>
+      <SignedIn>
+        <Suspense fallback={<Skeleton className="h-80 w-96 rounded-xl" />}>
+          <Documents />
+        </Suspense>
+      </SignedIn>
+      <SignedOut>
+        <div className="h-full w-full text-center text-3xl">
+          Sign in to use the dashboard!
+        </div>
+      </SignedOut>
     </div>
   );
 }
