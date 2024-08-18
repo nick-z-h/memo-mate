19a6b1930fe5ecd84a18898c8b817bc375adda89
nick-z-h <n.zhanghoule@gmail.com>
2024-08-17 21:49:01 -0400

    feat: clean home page and displaying on dashboard works

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,45 @@
+import { Suspense } from "react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "~/components/ui/card";
+
+import MarkdownFetcher from "~/components/markdown-fetcher";
+
+import { Skeleton } from "~/components/ui/skeleton";
+import React from "react";
+
+const mockUrls = [
+  "https://utfs.io/f/3b264d81-4160-495c-ba00-62b29f52479a-w1l1jd.mdx",
+  "https://utfs.io/f/6e6833eb-104c-4ef6-9ec8-3f2f7ab42b62-w1l1jc.mdx",
+  "https://utfs.io/f/72dfa17e-e39b-439f-b69c-065861c56255-w1l1jb.mdx",
+  "https://utfs.io/f/03933592-e1c1-4abc-94fa-01dc8f40ecd4-fgnuj6.mdx",
+];
+
+const mockDocuments = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
+export default function DashboardPage() {
+  return (
+    <div className="flex flex-wrap gap-4 text-wrap p-4">
+      <Suspense fallback={<Skeleton className="h-80 w-96 rounded-xl" />}>
+        {mockDocuments.map((document) => (
+          <Card key={document.id} className="h-80 w-96">
+            <CardHeader>
+              <CardTitle>Title</CardTitle>
+              <article className="prose lg:prose-xl h-64 w-80 overflow-auto">
+                <MarkdownFetcher url={document.url} />
+              </article>
+            </CardHeader>
+            <CardContent></CardContent>
+          </Card>
+        ))}
+      </Suspense>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,7 @@
-import Link from "next/link";
-import { Suspense } from "react";
-import MarkdownFetcher from "~/components/ui/markdown-fetcher";
-
-const mockUrls = [
-  "https://utfs.io/f/3b264d81-4160-495c-ba00-62b29f52479a-w1l1jd.mdx",
-  "https://utfs.io/f/6e6833eb-104c-4ef6-9ec8-3f2f7ab42b62-w1l1jc.mdx",
-  "https://utfs.io/f/72dfa17e-e39b-439f-b69c-065861c56255-w1l1jb.mdx",
-  "https://utfs.io/f/03933592-e1c1-4abc-94fa-01dc8f40ecd4-fgnuj6.mdx",
-];
-
-const mockDocuments = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        <Suspense fallback={<>Loading...</>}>
-          {mockDocuments.map((document) => (
-            <article key={document.id} className="prose lg:prose-xl">
-              <MarkdownFetcher url={document.url} />
-            </article>
-          ))}
-        </Suspense>
-      </div>
+      <div className="flex flex-wrap gap-4 text-wrap">Home Page</div>
     </main>
   );
 }
