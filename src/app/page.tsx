08706b07de14ac333fdd88f55bf87525bb84ec54
nick-z-h <n.zhanghoule@gmail.com>
2024-08-20 15:53:25 -0400

    chore: home page wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4 text-wrap">Home Page</div>
+      <div className="flex flex-wrap gap-4 text-wrap">Home Page WIP</div>
     </main>
   );
 }
