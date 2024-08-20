2a25f3ced2b6967d8cdfebb9c82d1b58a2e1ee7e
nick-z-h <n.zhanghoule@gmail.com>
2024-08-20 00:13:17 -0400

    bug(fix): handle error for when users upload images (revisit later)

--- src/components/markdown-fetcher.tsx
+++ src/components/markdown-fetcher.tsx
@@ -3,13 +3,18 @@ import React from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
 export default async function MarkdownFetcher({ url }: { url: string }) {
-  const res = await fetch(url);
+  try {
+    const res = await fetch(url);
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch the markdown file");
+    if (!res.ok) {
+      throw new Error("Failed to fetch the markdown file");
+    }
+
+    const markdown = await res.text();
+
+    return <MDXRemote source={markdown} />;
+  } catch (error) {
+    console.error(`Error fetching markdown from ${url}:`, error);
+    return <div>Failed to load content. Please try again later.</div>;
   }
-
-  const markdown = await res.text();
-
-  return <MDXRemote source={markdown} />;
 }
